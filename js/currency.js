4a8d089ce977d173d34bdb12d8a88c74bb4d6458
iakarshzz <abhiihun5432@gmail.com>
2026-07-28 21:10:09 +0530

    Update currency.js

--- js/currency.js
+++ js/currency.js
@@ -1,1 +1,30 @@
+const currencies = {
+    INR: {
+        name: "Indian Rupee",
+        symbol: "₹"
+    },
 
+    USD: {
+        name: "US Dollar",
+        symbol: "$"
+    },
+
+    EUR: {
+        name: "Euro",
+        symbol: "€"
+    },
+
+    GBP: {
+        name: "British Pound",
+        symbol: "£"
+    }
+};
+
+
+// Default Currency
+let currentCurrency = "INR";
+
+
+function getCurrencySymbol() {
+    return currencies[currentCurrency].symbol;
+}
